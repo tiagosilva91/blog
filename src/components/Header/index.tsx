81a7902d658cb96081b83e6b85ab3d41122ec481
Silva <tiago.g.silva@accenture.com>
2021-08-16 19:17:17 -0300

    Estilizacao Home concluida

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,14 @@
+import styles from './header.module.scss';
+import Link from 'next/link'
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.headerContent}>
+      <Link href="/">
+        <a>
+          <img src="/logo.svg" alt="logo" className={styles.logo} />
+        </a>
+      </Link>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,9 @@ import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi'
+import React from 'react';
 
 interface Post {
   uid?: string;
@@ -24,9 +27,78 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>
+          SpaceTraveling
+        </title>
+      </Head>
+      <main className={commonStyles.container}>
+        <div className={styles.posts}>
+          <a href="#" className={styles.post}>
+            <strong>Como Utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <ul>
+              <li>
+                <FiCalendar /> 15 Março 2021
+              </li>
+              <li>
+                <FiUser />Joseph Oliveira
+              </li>
+            </ul>
+          </a>
+          <a href="#" className={styles.post}>
+            <strong>Criar uma app CRA do zero</strong>
+            <p>Tudo sobre como criar a sua primeira aplicação utilizando Create React App</p>
+            <ul>
+              <li>
+                <FiCalendar /> 15 Março 2021
+              </li>
+              <li>
+                <FiUser />Joseph Oliveira
+              </li>
+            </ul>
+
+
+          </a>
+          <a href="#" className={styles.post}>
+            <strong>Como Utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <ul>
+              <li>
+                <FiCalendar /> 15 Março 2021
+              </li>
+              <li>
+                <FiUser />Joseph Oliveira
+              </li>
+            </ul>
+          </a>
+          <a href="#" className={styles.post}>
+            <strong>Criar uma app CRA do zero</strong>
+            <p>Tudo sobre como criar a sua primeira aplicação utilizando Create React App</p>
+            <ul>
+              <li>
+                <FiCalendar /> 15 Março 2021
+              </li>
+              <li>
+                <FiUser />Joseph Oliveira
+              </li>
+            </ul>
+          </a>
+          <button type="button">
+            Carregar mais posts
+          </button>
+        </div>
+
+
+      </main>
+    </>
+
+
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
